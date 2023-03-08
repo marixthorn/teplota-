34b316507fb13e8ed79d3c38bc6b8e3d217bf556
mari <125461169+marixthorn@users.noreply.github.com>
2023-03-08 13:32:13 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] poleT = new int [31];
+Console.WriteLine("Zadej teplotu");
+for (int i = 0; i < 31; i++) 
+{
+    poleT[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Co mám vypsat?");
+Console.WriteLine("Největší teplotu,nejmenší nebo průměr?");
+string teplota = Console.ReadLine();
+
+if ( teplota == "Nejmenší")
+
+{
+
+    int min = poleT[0];
+        foreach (int i in poleT)
+    {
+        min = Math.Min(min, i);
+    }
+
+    Console.WriteLine(min);
+}
+
+else if
+     (teplota == "Největší")
+
+    {
+
+        int max = poleT[0];
+        foreach (int i in poleT)
+        {
+           max = Math.Max (max, i);
+        }
+
+        Console.WriteLine(max);
+}
